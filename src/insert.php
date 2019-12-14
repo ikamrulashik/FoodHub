8c94fb977b2d8ada65c120b0c1433a2173f5cf5d
Kamrul Ashik <mdkamrul.islam@northsouth.edu>
2019-12-14 22:08:26 +0600

    changes

--- src/insert.php
+++ src/insert.php
@@ -4,44 +4,19 @@ $email=$_POST['email'];
 $password=$_POST['password'];
 $gender=$_POST['gender'];
 
-if(!empty($username) || !empty($email) || !empty($password) || !empty($gender)){
-	$host="localhost";
-	$dbUsername="root";
-	$dbPassword="";
-	$dbname="foodhub";
+//database connection
 
-	$conn=new mysqli($host, $dbUsername, $dbUsername, $name);
-
-	if (mysqli_connect_error()){
-		die('Connect Error('.mysqli_connect_error().')'.mysqli_connect_error());
-	}
-	else {
-		$SELECT ="SELECT email From signup Where email = ? Limit 1";
-		$INSERT = "INSERT Into signup (username, email, password, gender) values(?,?,?,?)";
-
-		$stmt=$conn->prepare($SELECT);
-		$stmt->bind_param("s", $email);
-		$stmt->execute();
-		$stmt->bind_result(&email);
-		$stmt->store_result();
-		$rnum=$stmt->num_rows;
-
-		if($rnum==0){
-			$stmt->close();
-
-			$stmt = $conn->prepare($INSERT);
-			$stmt->bind_param("sssssii", $username, $email, $password, $gender);
-			$stmt->execute();
-			echo "New record insertly succesfully";
-		}
-		else {
-			"Someone already register using this email";
-		}
-		$stmt->close();
-		$conn->close();
-	}
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+	die('Connection Failed : '.$conn->connect_error);
 }
 else{
-	echo "All field are required";
-	die();
+	$stmt = $sonn->prepare("insert into signup(name, email, password, gender)
+	values(?, ?, ?, ?)")
+	$stmt->bind_param("ssss", $name, $email, $password, $gender);
+	$stmt->execute();
+	echo "registration successfully...";
+	$stmt->close();
+	$conn->close();
 }
+?>
